616059e804067006e3a3032ed290c49b069fd279
dianedalyop <114318000+dianedalyop@users.noreply.github.com>
2023-12-16 05:32:22 +0000

    ca3 starter

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,33 @@
 package org.example;
 
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner carsreg = new Scanner(System.in);
 
+int cars ;
+            Deque<Integer> driveway = new ArrayDeque<>();
+            Deque<Integer> street = new ArrayDeque<>();
+        System.out.println("Pushing cars to the drive");
+        for(int i = 0 ; i<= 5; i++) {
+            System.out.println("Enter Car registration number");
+            cars = carsreg.nextInt();
+            driveway.push(cars);
+        }
+        System.out.println(driveway);
+        System.out.println("Pushing cars to street");
+        while(!driveway.isEmpty()){
+            
+
+
+
+
+        }
 
 
     }
